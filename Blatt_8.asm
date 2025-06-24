433a45173e04b4275d395544a1ac63c07b7c6bdf
saybar <saybar@stud.fra-uas.de>
2025-06-24 22:57:20 +0200

    sheet 8 task 3

--- Blatt_8.asm
+++ Blatt_8.asm
@@ -1,40 +1,38 @@
 .data
+
 # Eingabeaufforderung
 prompt:	.asciiz "Gib eine Zahl zwischen 1 und 3999 ein: "
 
 
-# RÃ¶mische Zahlentabelle â€“ Integerwerte
+# Roemische Zahlentabelle als Integerwerte
 values: .word 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
-symbols: M,CM, D, CD, C, XC, L, XL, X, IX, V, IV, I
+symbols: .word M,CM, D, CD, C, XC, L, XL, X, IX, V, IV, I
 
-# RÃ¶mische Symboltabelle â€“ Null-terminierte Strings
-
-            M:.asciiz "M"
-            CM:.asciiz "CM"
-            D:.asciiz "D"
-            CD:.asciiz "CD"
-            C:.asciiz "C"
-            XC:.asciiz "XC"
-            L:.asciiz "L"
-            XL:.asciiz "XL"
-            X:.asciiz "X"
-            IX:.asciiz "IX"
-            V:.asciiz "V"
-            IV:.asciiz "IV"
-            I:.asciiz "I"
+M:.asciiz "M"
+CM:.asciiz "CM"
+D:.asciiz "D"
+CD:.asciiz "CD"
+C:.asciiz "C"
+XC:.asciiz "XC"
+L:.asciiz "L"
+XL:.asciiz "XL"
+X:.asciiz "X"
+IX:.asciiz "IX"
+V:.asciiz "V"
+IV:.asciiz "IV"
+I:.asciiz "I"
             
 
 .text
-.globl main
 
-main:
+
 # Eingabeaufforderung ausgeben
 li $v0, 4
 la $a0, prompt
-syscall
+syscall	
 
 
-# String in Integer umwandeln
+# String als Integer einlesen
 li $v0, 5
 syscall
 move $t0, $v0
@@ -49,51 +47,24 @@ li $t5, 0  # der aktuelle Index i
 
 # Hier beginnt der Kern
 loop:
-beq $t5, $t4, done # Wenn i == 13 sind wir fertig
-lw $t6, 0($t1) # Ansonsten Speichern wir den aktuellen Value in temp6
-lw $t7, 0($t2)
-
+beq $t5, $t4, done      # Wenn i == 13 sind wir fertig
+lw $t6, 0($t1)          # Aktuellen Wert laden
+lw $t7, 0($t2)          # Aktuelle String-Adresse laden
+    
 repeat:
-blt $t6, $t0, next
-sub $t0, $t0, $t6
-
+blt $t0, $t6, next      # Wenn zahl < wert, zum nächsten
+sub $t0, $t0, $t6       # zahl -= wert
 li $v0, 4
 move $a0, $t7
 syscall
-
 j repeat
-
-
+    
 next:
-addiu $t1, $t1, 4
-addiu $t2, $t2, 4
+addiu $t1, $t1, 4       # Zum nächsten Wert
+addiu $t2, $t2, 4       # Zur nächsten String-Adresse  
+addiu $t5, $t5, 1       # Index erhöhen!
+j loop                  # Zurück zum Loop-Anfang
 
 done:
-li $v0, 10
-syscall
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Programm beenden
 
